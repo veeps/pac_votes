eb6d02f002c6db1b640884bd06458460e573ea74
veeps <vpeng12@gmail.com>
2024-08-13 12:22:37 -0700

    removing unnecessary merge step

--- src/get_summary_table.py
+++ src/get_summary_table.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
 
-def get_summary_table(vote):
+def get_summary_table(vote, df):
     # Generate summary table
-    grouped_df = df[df['CAND_VOTE'] == vote].groupby(['CAND_NAME']).agg({'TRANSACTION_AMT': 'sum'}).reset_index().sort_values(by = "TRANSACTION_AMT", ascending = False)
-    grouped_df = grouped_df.merge(yea[['CAND_NAME', 'CAND_PTY_AFFILIATION']], how = "left")
+    grouped_df = df[df['CAND_VOTE'] == vote].groupby(['CAND_NAME', 'CAND_PTY_AFFILIATION']).agg({'TRANSACTION_AMT': 'sum'}).reset_index().sort_values(by = "TRANSACTION_AMT", ascending = False)
     grouped_df = grouped_df.rename(columns={'CAND_NAME': 'Candidate Name',
-                                            'TRANSACTION_AMT': 'Total donations received from insurance PACS 2020-2024',
+                                            'TRANSACTION_AMT': 'Total $ received from insurance PACs 2020-2024',
                                             'CAND_PTY_AFFILIATION': 'Candidate Party'})
     return grouped_df
-
