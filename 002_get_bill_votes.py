aafc481c8996500099480dbc55ac23500b2ef0a7
veeps <vpeng12@gmail.com>
2024-08-13 14:51:31 -0700

    updating ignore file

--- 002_get_bill_votes.py
+++ 002_get_bill_votes.py
@@ -4,15 +4,20 @@ import json
 import sys
 
 with open('project_config.json','r') as fp: 
+    
     project_config = json.load(fp)
  
 module_path = os.path.join(project_config['project_module_relative_path'])
 sys.path.append(module_path)
 import get_votes
+import get_summary_table
+
 from importlib import reload
 from get_votes import * 
+from get_summary_table import * 
+
  
-reload(get_votes)
+reload(get_summary_table)
 
 
 ############## Read in votes for bill
@@ -33,5 +38,16 @@ df.drop(columns=['CAND_OFFICE_ST_v'], inplace = True)
 # Rename column
 df = df.rename(columns={'CAND_ST_p': 'CAND_ST'})
 
+grouped_df = df[df['CAND_VOTE'] == "yea"].groupby(['CAND_NAME', 'CAND_PTY_AFFILIATION']).agg({'TRANSACTION_AMT': 'sum'}).reset_index().sort_values(by = "TRANSACTION_AMT", ascending = False)
+grouped_df = grouped_df.rename(columns={'CAND_NAME': 'Candidate Name',
+                                            'TRANSACTION_AMT': 'Total $ received from insurance PACs 2020-2024',
+                                            'CAND_PTY_AFFILIATION': 'Candidate Party'})
+
+grouped_df
+
+
 # Generate summary table
-test = get_summary_table("yea")
+test = get_summary_table("yea", df)
+
+
+df
