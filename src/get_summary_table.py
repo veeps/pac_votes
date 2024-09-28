e28c68925fb5e3b55a7f79454a35998a9b2ea581
veeps <vpeng12@gmail.com>
2024-09-28 15:13:28 -0700

    updating to 1995 data

--- src/get_summary_table.py
+++ src/get_summary_table.py
@@ -5,6 +5,6 @@ def get_summary_table(vote, df):
     # Generate summary table
     grouped_df = df[df['CAND_VOTE'] == vote].groupby(['CAND_NAME', 'CAND_PTY_AFFILIATION']).agg({'TRANSACTION_AMT': 'sum'}).reset_index().sort_values(by = "TRANSACTION_AMT", ascending = False)
     grouped_df = grouped_df.rename(columns={'CAND_NAME': 'Candidate Name',
-                                            'TRANSACTION_AMT': 'Total $ received from insurance PACs 2020-2024',
+                                            'TRANSACTION_AMT': 'Total $ received from insurance PACs 1995-2024',
                                             'CAND_PTY_AFFILIATION': 'Candidate Party'})
     return grouped_df
--- src/get_votes.py
+++ src/get_votes.py
@@ -51,6 +51,7 @@ def get_rollcall_votes(url):
 
 def get_senate_votes(url):
     # Fetch the XML file from the URL
+    response = requests.get(url)
     response.raise_for_status()  # Raise an error for bad status codes
 
     # Parse the XML content
